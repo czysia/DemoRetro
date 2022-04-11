12a6e7616af66194d334809629ea28a30c2a3a35
czysia <nson02902@gmail.com>
2022-04-11 21:04:22 +0700

    rtest next

--- app/src/main/java/com/example/mob403demo3retro/ApiUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.mob403demo3retro;
-
-public class ApiUtils {
-    public static final String BASE_URL = "https://flying-blossom-cerise.glitch.me/";
-
-    public static ApiService getApiService() {
-        return com.example.app_ban_hang_tot_nghiep.RetrofitUtils.getClient(BASE_URL).create(ApiService.class);
-    }
-}
